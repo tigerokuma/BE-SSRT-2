a120c4160fb8a58b520e07f15b9f4fe7f927cf94
tigerokuma <okanagantiger@gmail.com>
2025-07-08 13:16:33 -0700

    feat: Added prisma.module.ts to make it global

--- ossrepo-backend/src/common/prisma/prisma.service.ts
+++ ossrepo-backend/src/common/prisma/prisma.service.ts
@@ -3,10 +3,11 @@ import { PrismaClient } from '../../../generated/prisma';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  // Connects to database when app starts
   async onModuleInit() {
     await this.$connect();
   }
-
+  // Disconnects from database when app stops
   async onModuleDestroy() {
     await this.$disconnect();
   }
