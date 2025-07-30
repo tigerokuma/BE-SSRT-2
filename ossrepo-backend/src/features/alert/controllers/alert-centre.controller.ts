c5a194327f4f812a8835cd6ccd2708406fe26b2d
Stephen Ehebald <ehebaldstephen@gmail.com>
2025-07-29 18:13:11 -0700

    fix: update alert centre to use new schema

--- ossrepo-backend/src/features/alert/controllers/alert-centre.controller.ts
+++ ossrepo-backend/src/features/alert/controllers/alert-centre.controller.ts
@@ -1,28 +1,18 @@
-import { Controller, Get, Post, Patch, Delete } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AlertCentreService } from  '../services/alert-centre.service';
-import { UpdateAlertDto } from '../dto/create-alert.dto';
 
-@Controller('alert_centre/alert')
+@Controller('alert_centre')
 export class AlertCentreController {
   constructor(private readonly service: AlertCentreService) {}
 
-  @Get()
-  getAlert(body: any) {
-    return this.service.getAlert(body);
+  @Get("alert/:alert_id")
+  getAlert(@Param('alert_id') alert_id: string) {
+    return this.service.getAlert(alert_id);
   }
 
-  @Patch()
-  updateAlert(alert_id: string, updateAlertDto: UpdateAlertDto) {
-    return this.service.updateAlert(alert_id, updateAlertDto);
+  @Get("user/:user_id")
+  getUserAlerts(@Param("user_id") user_id: string) {
+    return this.service.getUserAlerts(user_id);
   }
 
-  @Delete()
-  deletesAlert(body: any) {
-    return this.service.deletesAlert(body);
-  }
-
-  @Post()
-  createAlert(userWatchlistId: string, body: any) {
-    return this.service.createAlert(userWatchlistId, body);
-  }
 }
--- ossrepo-backend/src/features/alert/repositories/alert.repository.ts
+++ ossrepo-backend/src/features/alert/repositories/alert.repository.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../common/prisma/prisma.service';
-import { CreateAlertDto, UpdateAlertDto } from '../dto/create-alert.dto';
 
 @Injectable()
 export class AlertCentreRepository {
@@ -12,35 +11,13 @@ export class AlertCentreRepository {
     });
   }
 
-  async createAlert(ID: string, createAlertDto: CreateAlertDto) {
-    const latestAlert = await this.prisma.alert.findFirst({
-        where: { id: ID },
-        orderBy: { alert_id: 'desc' },
-        select: { alert_id: true },
-      });
-
-    const newAlertId = latestAlert ? latestAlert.alert_id + 1 : 1;
-
-    return this.prisma.alert.create({ data: {
-     alert_id: newAlertId.toString(),
-     id: ID,
-     risk: createAlertDto.risk,
-     status: createAlertDto.status,
-     title: createAlertDto.title,
-     description: createAlertDto.description,
-    } });
-  }
-
-  async updateGeneralAlert(alertID: string, updateAlertDto: UpdateAlertDto) {
-    return this.prisma.alert.updateMany({
-      where: { alert_id: alertID },
-      data: updateAlertDto,
+  async getUserAlerts(user_watchlist_id: string) {
+    return await this.prisma.alertTriggered.findMany({
+        where: {user_watchlist_id: user_watchlist_id},
+        orderBy: {
+          created_at: 'desc'
+        }
     });
   }
 
-  async deleteGeneralAlert(alert_id: string) {
-    return this.prisma.alert.deleteMany({
-      where: { alert_id },
-    });
-  }
 }
