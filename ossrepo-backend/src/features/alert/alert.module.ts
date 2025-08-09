26e801e801756c41c4ba4d5d6ab18dcfa190d115
Stephen Ehebald <ehebaldstephen@gmail.com>
2025-08-09 00:53:28 -0700

    feat: get rid of alerts in alert-centre

--- ossrepo-backend/src/features/alert/alert.module.ts
+++ ossrepo-backend/src/features/alert/alert.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AlertCentreController } from './controllers/alert-centre.controller';
 import { EmailController } from './controllers/email.controller';
 import { SlackController } from './controllers/slack.controller';
 import { JiraController } from './controllers/jira.controller';
-import { AlertCentreService } from './services/alert-centre.service';
 import { EmailService } from './services/email.service';
 import { SlackService } from './services/slack.service';
 import { JiraService } from './services/jira.service';
@@ -11,27 +9,25 @@ import { ConfigModule } from '@nestjs/config';
 import { SlackRepository } from './repositories/slack.repository';
 import { JiraRepository } from './repositories/jira.repository';
 import { EmailRepository } from './repositories/email.repository';
-import { AlertCentreRepository } from './repositories/alert.repository'
+import { UserModule } from '../user/user.module';
 import { ScheduleModule } from '@nestjs/schedule';
 
 
 @Module({
   imports: [
     ConfigModule,
+    UserModule,
     ScheduleModule.forRoot(),
   ],
   controllers: [
-    AlertCentreController,
     EmailController,
     SlackController,
     JiraController
   ],
   providers: [
-    AlertCentreService,
     EmailService,
     SlackService,
     JiraService,
-    AlertCentreRepository,
     SlackRepository,
     JiraRepository,
     EmailRepository
